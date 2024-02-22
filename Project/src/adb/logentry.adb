a7fae7556b39a7406d22bc5e6bf24e464714df89
Alberto Angeloni <alberto.angeloni@studenti.unipd.it>
2024-02-22 17:33:52 +0100

    asdrubale

--- Project/src/adb/logentry.adb
+++ Project/src/adb/logentry.adb
@@ -12,9 +12,4 @@ package body LogEntry is
            Left.Index = Right.Index and Left.State = Right.State and
            Left.Term = Right.Term;
     end "=";
-
-    procedure Set_State(obj : in out LogEntry; state : LogEntryState) is
-        begin
-            obj.State:=state;
-        end Set_State;
 end LogEntry;
--- Project/src/ads/logentry.ads
+++ Project/src/ads/logentry.ads
@@ -2,17 +2,14 @@ with Payload;
 
 package LogEntry is
 
-    type LogEntryState is (APPENDED, COMMITTED);
 
     type LogEntry is record
         Term    : Integer;
         Index   : Integer;
         Peyload : Payload.Payload;
-        State   : LogEntryState := APPENDED;
     end record;
 
     function Entry_Stringify (obj : in LogEntry) return String;
     function "=" (Left, Right : LogEntry) return Boolean;
-    procedure Set_State(obj : in out LogEntry; state : LogEntryState);
 
 end LogEntry;
